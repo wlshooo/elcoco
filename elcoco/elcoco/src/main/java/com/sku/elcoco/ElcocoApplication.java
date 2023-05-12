2d310083e3e3fb7b8bf1791b0c0de98176b9f915
jinhoLee <kswlsgh@naver.com>
2023-05-13 05:01:05 +0900

    mysql 연동

---
+++ elcoco/elcoco/src/main/java/com/sku/elcoco/ElcocoApplication.java
@@ -0,0 +1,13 @@
+package com.sku.elcoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElcocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElcocoApplication.class, args);
+	}
+
+}
---
+++ elcoco/elcoco/src/test/java/com/sku/elcoco/ElcocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sku.elcoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElcocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
