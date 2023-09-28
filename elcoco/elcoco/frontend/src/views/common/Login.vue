9b0aec7c619439d531f0ebb2ffaa16719b807536
jinhoLee <kswlsgh@naver.com>
2023-09-28 15:18:33 +0900

    refactor: login 창 front 수정

--- elcoco/elcoco/frontend/src/views/common/Login.vue
+++ elcoco/elcoco/frontend/src/views/common/Login.vue
@@ -1,108 +1,132 @@
 <template>
-    <div>
-        <div class="container my-5">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h2 class="card-title text-center"><strong>로그인</strong></h2>
-                            <hr>
-
-                            <form @submit.prevent="fnLogin">
-                                <div class="form-group row mb-3">
-                                    <label for="uid" class="col-md-4 col-form-label text-md-right">아이디</label>
-                                    <div class="col-md-8">
-                                        <input id="uid" class="form-control" name="uid" placeholder="아이디를 입력해주세요."
-                                               ref="idInput" v-model="user_id">
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-3">
-                                    <label for="password" class="col-md-4 col-form-label text-md-right">비밀번호</label>
-                                    <div class="col-md-8">
-                                        <input id="password" name="password" class="form-control"
-                                               placeholder="비밀번호를 입력해주세요."
-                                               ref="passwordInput" v-model="user_pw" type="password">
-                                    </div>
-                                </div>
-                                <hr>
-
-                                <div class="form-group text-center mt-3">
-                                    <button type="submit" class="btn btn-primary btn-block">로그인</button>
-                                </div>
-
-                                <div class="form-group text-center mt-3">
-                                    <router-link to="/signup">아직 회원이 아니신가요?</router-link>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
+  <div>
+    <div class="container my-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title text-center"><strong>로그인</strong></h2>
+              <br>
+              <h2 class="card-subtitle text-center">어서 오세요! 환영합니다.</h2>
+              <br><br>
+              <form @submit.prevent="fnLogin">
+                <div class="form-group">
+                  <label for="uid" class="col-md-4 col-form-label"><strong>아이디</strong></label>
+                  <div class="input-group">
+                    <input id="uid" class="form-control rounded-left" name="uid" placeholder="아이디를 입력해주세요."
+                           ref="idInput" v-model="user_id">
+                  </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</template>
+                <br>
+                <div class="form-group">
+                  <label for="password" class="col-form-label"><strong>비밀번호</strong></label>
+                  <div class="input-group">
+                    <input id="password" name="password" class="form-control rounded-left" placeholder="비밀번호를 입력해주세요."
+                           ref="passwordInput" v-model="user_pw" type="password">
+                  </div>
+                </div>
 
+                <br><br>
+                <div class="form-group text-center mt-3">
+                  <!-- 버튼 스타일 조정 -->
+                  <button type="submit" class="btn btn-primary btn-block my-button"><strong>로그인</strong></button>
+                </div>
+
+                <div class="form-group text-center mt-3">
+                  <router-link to="/signup">아직 회원이 아니신가요?</router-link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped>
 .card {
-    border: none;
+  border: none;
 }
 
 .card-title {
-    font-size: 24px;
+  font-size: 24px;
+}
+
+.card-subtitle {
+  font-size: 20px;
 }
 
 .btn-primary {
-    background-color: #007bff;
-    border: none;
+  background-color: #BBDEFB;
+  border: none;
+  color: black;
 }
+
+/* input을 둥글게 만들기 */
+.input-group .form-control {
+  border-radius: 25px;
+  height: 45px;
+  /* 아래 라인 추가 */
+  width: 100%; /* 너비를 100%로 설정하여 input의 너비와 동일하게 만듭니다. */
+}
+
+/* 버튼 크기 및 폰트 크기 조정 */
+.my-button {
+  border-radius: 25px;
+  height: 45px;
+  font-size: 16px;
+  /* 아래 라인 추가 */
+  width: 100%; /* 너비를 100%로 설정하여 input의 너비와 동일하게 만듭니다. */
+}
+
 </style>
 
-
 <script>
-import {mapActions, mapGetters} from 'vuex'   //vuex 추가
+import {mapActions, mapGetters} from 'vuex'
 
 export default {
-    data() {
-        return {
-            user_id: '',
-            user_pw: ''
-        }
-    },
-    methods: {
-        ...mapActions(['login']),     //vuex/actions에 있는 login 함수
-
-        async fnLogin() {       //async 함수로 변경
-            if (this.user_id === '') {
-                alert('아이디를 입력해주세요.');
-                this.$refs.idInput.focus();
-                return;
-            }
-            if (this.user_pw === '') {
-                alert('비밀번호를 입력해주세요.');
-                this.$refs.passwordInput.focus();
-                return;
-            }
-
-            try {
-                let loginResult = await this.login({user_id: this.user_id, user_pw: this.user_pw})
-                if (loginResult) this.goToPages()
-            } catch (err) {
-                alert("아이디와 패스워드를 다시 확인해주세요")
-                location.reload()
-            }
-        },
-        goToPages() {
-            this.$router.push({
-                name: 'BoardList'
-            })
-        }
-    },
-    computed: {
-        ...mapGetters({
-            errorState: 'getErrorState'
-        })
+  data() {
+    return {
+      user_id: '',
+      user_pw: ''
     }
+  },
+  methods: {
+    ...mapActions(['login']),
+    async fnLogin() {
+      if (this.user_id === '') {
+        alert('아이디를 입력해주세요.')
+        this.$refs.idInput.focus()
+        return
+      }
+      if (this.user_pw === '') {
+        alert('비밀번호를 입력해주세요.')
+        this.$refs.passwordInput.focus()
+        return
+      }
+
+      try {
+        let loginResult = await this.login({
+          user_id: this.user_id,
+          user_pw: this.user_pw
+        })
+        if (loginResult) this.goToPages()
+      } catch (err) {
+        alert('아이디와 패스워드를 다시 확인해주세요')
+        location.reload()
+      }
+    },
+    goToPages() {
+      this.$router.push({
+        name: 'BoardList'
+      })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      errorState: 'getErrorState'
+    })
+  }
 }
 </script>
